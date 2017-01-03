4f16b46bbf787361686b9c37ee38a2cf3c16e921
lincolnge <326684793@qq.com>
2017-01-03 17:14:51 +0800

    Update: update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -4,13 +4,21 @@ const entry = {
   main: './src/main',
   main2: './src/main2'
 };
+
+const commonsPlugin = new webpack.optimize.CommonsChunkPlugin('common.js');
+let plugins = [];
+
+console.log('__dirname', __dirname, process.env.isOptimized);
+
+if (Boolean(process.env.isOptimized) === true) {
+  plugins.push(commonsPlugin);
+}
+
 module.exports = {
   entry,
   output: {
     filename: 'bundle.[name].js',
     path:'./dist'
   },
-  plugins: [
-    new webpack.optimize.CommonsChunkPlugin('common.js', ['main', 'main2'])
-  ]
+  plugins
 };
