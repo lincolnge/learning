39e6e71de0b6f4829d4506e197c151f55ab5fcbe
lincolnge <326684793@qq.com>
2017-05-18 23:48:37 +0800

    Add: add oo.

--- JavaScript/oo.js
+++ JavaScript/oo.js
@@ -12,15 +12,18 @@ Rectangle.prototype.toString = function() {
 }
 
 function Square(size) {
-	this.length = size;
-	this.width = size;
+	// 0-1
+	// this.length = size;
+	// this.width = size;
+	// 0-2
+	Rectangle.call(this, size, size);
 }
 
-// 1
+// 1-1
 Square.prototype = new Rectangle();
 Square.prototype.constructor = Square;
 
-// 2
+// 1-2
 // Square.prototype = Object.create(Rectangle.prototype, {
 // 	constructor: {
 // 		configurable: true,
@@ -31,7 +34,11 @@ Square.prototype.constructor = Square;
 // });
 
 Square.prototype.toString = function() {
-	return "[Square " + this.length + "x" + this.width + "]";
+	// 2-1
+	// return "[Square " + this.length + "x" + this.width + "]";
+	// 2-2
+	var text = Rectangle.prototype.toString.call(this);
+	return text.replace('Rectangle', 'Square');
 }
 
 var rect = new Rectangle(5, 10);
