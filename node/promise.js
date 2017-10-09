5a6ce7d379280ad5f4c5796932e57c7aea32be30
lincolnge <326684793@qq.com>
2017-10-09 20:09:02 +0800

    Add: add promise.

--- node/promise.js
+++ node/promise.js
@@ -66,14 +66,42 @@ var readFile1 = function(file, encoding) {
   fs.readFile(file, encoding, deferred.callback());
   return deferred.promise;
 }
-var readFile2 = function(file, encoding) {
-  var deferred = new Deferred();
-  fs.readFile(file, encoding, deferred.callback());
-  return deferred.promise;
+// var readFile2 = function(file, encoding) {
+//   var deferred = new Deferred();
+//   fs.readFile(file, encoding, deferred.callback());
+//   return deferred.promise;
+// }
+// readFile1('t.txt', 'utf8').then(function(file1) {
+//   console.log('file1', file1.trim());
+//   return readFile2(file1.trim(), 'utf8');
+// }).then(function(file2) {
+//   console.log('file2', file2);
+// })
+
+// readFile1('t.txt', 'utf8').then(function(file1) {
+//   console.log('file1', file1.trim());
+//   return readFile1(file1.trim(), 'utf8');
+// }).then(function(file2) {
+//   console.log('file2', file2);
+// })
+
+var smooth = function(method) {
+  return function() {
+    var deferred = new Deferred();
+    var args = Array.prototype.slice.call(arguments);
+    args.push(deferred.callback());
+    method.apply(null, args);
+    console.log('smooth1', args, Array.prototype.slice.call(arguments));
+    return deferred.promise;
+  }
 }
-readFile1('t.txt', 'utf8').then(function(file1) {
-  console.log('file1', file1.trim());
-  return readFile2(file1.trim(), 'utf8');
+
+var readFile = smooth(fs.readFile);
+
+console.log('smooth')
+readFile('t.txt', 'utf8').then(function(file1) {
+  console.log('file1', file1);
+  return readFile(file1.trim(), 'utf8');
 }).then(function(file2) {
   console.log('file2', file2);
 })
