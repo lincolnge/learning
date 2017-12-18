176248a82106fb17d855c5c49d32d995a04bea1d
lincolnge <326684793@qq.com>
2017-12-18 20:26:43 +0800

    Update: 更新句柄传递

--- node/multithreading/child.js
+++ node/multithreading/child.js
@@ -1,7 +1,13 @@
-process.on('message', function(m, server) {
+var http = require('http');
+var server = http.createServer(function(req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('handled by child, pid is ' + process.pid + '\n');
+})
+process.on('message', function(m, tcp) {
   if (m === 'server') {
-    server.on('connection', function(socket) {
-      socket.end('handled by child, pid is ' + process.pid + '\n');
+    tcp.on('connection', function(socket) {
+      // socket.end('handled by child, pid is ' + process.pid + '\n');
+      server.emit('connection', socket);
     });
   }
 });
--- node/multithreading/parent.js
+++ node/multithreading/parent.js
@@ -4,9 +4,12 @@ var child2 = cp.fork('child.js');
 
 var server = require('net').createServer();
 server.on('connection', function(socket) {
+  // 关闭父进程后，这里不会再执行。
   socket.end('handled by parent\n');
 });
 server.listen(1337, function() {
   child1.send('server', server);
   child2.send('server', server);
+  // 关掉父进程，完全让子进程处理。
+  server.close();
 })
