89d149b295b2daa7d05075b61b27cb224971bc1b
lincolnge <326684793@qq.com>
2018-01-02 10:33:29 +0800

    Update: update net js which contain transfer encoding.

--- node/web/net.js
+++ node/web/net.js
@@ -1,14 +1,29 @@
 var net = require('net');
 
-var server = net.createServer(function(socket) {
-  socket.on('data', function(data) {
-    socket.write('hello 123');
-  });
+/**
+ * HTTP 协议中的 Transfer-Encoding
+ * https://imququ.com/post/transfer-encoding-header-in-http.html
+ */
+var server = net.createServer(function(sock) {
+  sock.on('data', function(data) {
+    sock.write('HTTP/1.1 200 OK\r\n');
+    sock.write('Transfer-Encoding: chunked\r\n');
+    sock.write('\r\n');
 
-  socket.on('end', function() {
-    console.log('end 123');
-  });
-  socket.write('welcome!! 123');
+    sock.write('b\r\n');
+    sock.write('01234567890\r\n');
+
+    sock.write('5\r\n');
+    sock.write('12345\r\n');
+
+    sock.write('0\r\n');
+    sock.write('\r\n');
+});
+
+  // socket.on('end', function() {
+  //   console.log('end 123');
+  // });
+  // socket.write('welcome!! 123');
 
   // socket.pipe(socket);
 });
