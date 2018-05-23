0415f2d38e1190e8ad6e1dd10a7d330005219b13
lincolnge <326684793@qq.com>
2018-05-23 17:46:00 +0800

    Add: create a sample tree.

---
+++ algorithm/root-tree.js
@@ -0,0 +1,37 @@
+/**
+ * 创建一个树的基本结构。
+ * tree：
+
+        2
+       / \
+      /   \
+     /     \
+    1       3
+   / \     / \
+  0   7   9   1
+ /   / \     / \
+2   1   0   8   8
+       /
+      7
+
+ */
+
+function TreeNode(val) {
+  this.val = val;
+  this.left = null;
+  this.right = null;
+}
+
+var rootTree = new TreeNode(2);
+rootTree.left = new TreeNode(1);
+rootTree.left.left = new TreeNode(0);
+rootTree.left.right = new TreeNode(7);
+rootTree.left.right.left = new TreeNode(1);
+rootTree.left.right.right = new TreeNode(0);
+rootTree.left.right.right.left = new TreeNode(7);
+rootTree.left.left.left = new TreeNode(2);
+rootTree.right = new TreeNode(3);
+rootTree.right.left = new TreeNode(9);
+rootTree.right.right = new TreeNode(1);
+rootTree.right.right.left = new TreeNode(8);
+rootTree.right.right.right = new TreeNode(8);
