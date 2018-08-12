c977986daa190e729fe1a7eb2c702fc03e8c6bdf
lincolnge <326684793@qq.com>
2018-08-13 01:16:06 +0800

    Update: update java code.

---
+++ Java/src/com/cat/Main.java
@@ -0,0 +1,14 @@
+package com.cat;
+
+//import com.cat.MyClass; // 不需要这一句。
+
+public class Main {
+
+    public static void main(String[] args) {
+        if (1 == 1) {
+            System.out.println(MyClass.addNumbers(1, 2));
+        }
+
+    }
+
+}
---
+++ Java/src/com/cat/MyClass.java
@@ -0,0 +1,7 @@
+package com.cat;
+
+public class MyClass {
+    public static int addNumbers(int a, int b) {
+        return a + b;
+    }
+}
