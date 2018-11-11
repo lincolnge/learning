18bb21612b4ff2392abe0fbcd4888ad4292e8e4b
lincolnge <326684793@qq.com>
2018-11-11 15:03:43 +0800

    Add: add string-to-integer-atoi.js

---
+++ algorithm/string-to-integer-atoi.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} str
+ * @return {number}
+ * https://leetcode.com/problems/string-to-integer-atoi/
+ */
+var myAtoi = function(str) {
+    var result = parseInt(str, 10);
+    if (isNaN(result)) {
+        return 0;
+    }
+    if (result >= Math.pow(2, 31)) {
+        return Math.pow(2, 31) - 1;
+    } else if (result < -Math.pow(2, 31)) {
+        return -Math.pow(2, 31);
+    }
+    return result;
+};
+
+console.log(myAtoi('42'), 42);
+console.log(myAtoi('+-42'), 0);
+console.log(myAtoi('     -42'), -42);
+console.log(myAtoi('4193 with words'), 4193);
+console.log(myAtoi('words and 987'), 0);
+console.log(myAtoi('-91283472332'), -2147483648);
+
+/**
+ * https://segmentfault.com/a/1190000010571914
+ * JavaScript 实现 parseInt()
+ */
